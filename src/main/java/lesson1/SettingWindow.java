834cc8ae28143988dbf8864f17ce5e0ae001f7b7
Oksana <Ksanka82@yandex.ru>
2023-11-15 11:13:36 +0300

    Lesson1

---
+++ src/main/java/lesson1/SettingWindow.java
@@ -0,0 +1,28 @@
+package lesson1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SettingWindow extends JFrame {
+    private static final int WINDOW_HEIGHT = 230;
+    private static final int WINDOW_WIDTH = 350;
+
+
+    JButton btnStart = new JButton("Start new game");
+
+    SettingWindow(GameWindow gameWindow) {
+        setLocationRelativeTo(gameWindow);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameWindow.startNewGame(0, 3, 3, 3);
+                setResizable(false);
+            }
+        });
+
+        add(btnStart);
+    }
+
+}
